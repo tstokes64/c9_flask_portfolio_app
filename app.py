4013be20863a26df5371910425fb725127334f0f
tstokes64 <45905879+tstokes64@users.noreply.github.com>
2018-12-15 20:22:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home_page():
-	new_name = name + "likes to eat mangos."
-	return render_template('index.html', name=new_name)
+	return render_template('index.html')
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name)
+	new_name = name + "likes to eat mangos."
+	return render_template('index.html', name=new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
